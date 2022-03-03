e49b852a7c9f1a5470428eda9c39ab3c4100fb53
Robert21000 <2404145860314@ingenieria.usac.edu.gt>
2022-03-02 22:47:48 -0600

    login server

--- datos/web/index.js
+++ datos/web/index.js
@@ -55,4 +55,36 @@ app.get("/api/clientes",async (req, res)=> {
       });
   });
 
+
+  app.post("/api/login",async (req, res)=> {
+	let body=req.body;
+	let pass= body.pass;
+	let correo= body.correo;
+    const consulta = connection.query('Select * from Cliente where(correo=\''+correo+'\' and contrasenia=\''+pass+'\' )',function (err, rows) {
+        if (err) throw err;
+        let bandera=false
+		if(rows.length==1){
+			bandera=true
+			res.json({estado:bandera,Usuario:{id_cliente:rows[0].id_cliente,nombre:rows[0].nombre,apellido:rows[0].apellido,correo:rows[0].correo,celular:rows[0].celular,fecha_nac:rows[0].fecha_nacm,foto:rows[0].foto,tipox:"cliente"}});
+		}else{
+
+			const consulta = connection.query('Select * from Anfitrion where(correo=\''+correo+'\' and contrasenia=\''+pass+'\' )',function (err, rows) {
+				if (err) throw err;
+				let bandera=false
+				if(rows.length==1){
+					bandera=true
+					res.json({id_anfitrion:rows[0].id_anfitrion,nombre:rows[0].nombre,apellido:rows[0].apellido,tipo:rows[0].tipo,empresa:rows[0].empresa,dpi:rows[0].dpi,correo:rows[0].correo,foto:rows[0].foto,direccion:rows[0].direccion,estado:rows[0].estado,n_instancias:rows[0].n_instancias,valoracion:rows[0].valoracion});
+				}else{
+							
+					res.json({tipox:"null"});
+				}
+				
+			});
+			//res.json({estado:bandera,Usuario:""});
+		}
+		
+		//res.json(rows);
+      });
+  });
+
 app.listen(5000, () => console.log('listining on port 5000'));
