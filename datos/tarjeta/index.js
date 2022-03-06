e7c35ee40d8f97701d5ac7ce641989129161182e
Robert21000 <2404145860314@ingenieria.usac.edu.gt>
2022-03-05 23:36:33 -0600

    nueva configuracion

---
+++ datos/tarjeta/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const mysql = require('mysql');
+const nodemailer = require("nodemailer");
+const app = express();
+const dateShortcode = require('date-shortcode')
+
+const fileUpload = require('express-fileupload')
+var cors = require('cors')
+var morgan=require('morgan')
+var bodyParser = require('body-parser')
+
+
+app.use(express.static('files'));
+//app.use(bodyParser());
+app.use(fileUpload())
+app.use(cors())
+
+app.use(bodyParser.json({limit: "90mb"}));
+app.use(bodyParser.urlencoded({limit: "90mb", extended: true, parameterLimit:50000}));
+
+app.use(morgan("dev"));
+
+
+
+
+const connection = mysql.createPool({
+	connectionLimit: 10,
+	host: process.env.MYSQL_HOST || 'localhost',
+	user: process.env.MYSQL_USER || 'root',
+	password: process.env.MYSQL_PASSWORD || '1234',
+	database: process.env.MYSQL_DATABASE || 'test'
+});
+
+
+
+  app.get("/api/propiedades/anfitrion/:id",async (req, res)=> {
+	let id=req.params.id;
+    const insert = connection.query('Select * from Propiedad where (id_anfitrion='+id+')',function (err, rows) {
+        if (err) throw err;
+        res.json(rows);
+      });
+  });
+
+
+
+
+  app.post("/api/cards/",async (req, res)=> {
+		let numero= req.body.numero;
+        let contrasenia= req.body.contrasenia;
+        let tipo= req.body.tipo;
+        let saldo= req.body.saldo;
+        let id_cliente= req.body.id_cliente;
+
+	 let consulta='Insert into Tarjeta(numero,contrasenia,tipo,saldo,id_cliente) values('
+	 connection.query(consulta+''+numero+',\''+contrasenia+'\',\''+tipo+'\','+saldo+','+id_cliente+')',function (err, rows) {
+        if (err) throw err;	
+		res.json(rows);
+		
+      });
+  });
+  
+
+
+app.listen(3000, () => console.log('listining on port 3000'));
